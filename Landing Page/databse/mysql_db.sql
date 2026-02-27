b4d21ed304040dd5876e6dc33ab697bfb7957b4f
krishan20221840 <krishan.20221840@iit.ac.lk>
2026-02-27 16:02:17 +0530

    Update mysql_db.sql
    added driver details table

--- Landing Page/databse/mysql_db.sql
+++ Landing Page/databse/mysql_db.sql
@@ -4,5 +4,14 @@
 CREATE TABLE `bus` (
     `bus_id` int(11) NOT NULL,
     `bus_number_plate` varchar(15) NOT NULL,
-    `capacity` int(11) NOT NULL,
+    `capacity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+CREATE TABLE `driver_details` (
+    `driver_id` int(11) NOT NULL,
+    `full_name` mediumtext NOT NULL,
+    `mobile_number` int(10) NOT NULL,
+    `driving license number` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
