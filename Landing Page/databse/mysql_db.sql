7cd9a68854e7e0e20fdde856358fa1eddb5d82c0
krishan20221840 <krishan.20221840@iit.ac.lk>
2026-02-27 15:55:43 +0530

    db table bus

---
+++ Landing Page/databse/mysql_db.sql
@@ -0,0 +1,8 @@
+
+
+
+CREATE TABLE `bus` (
+    `bus_id` int(11) NOT NULL,
+    `bus_number_plate` varchar(15) NOT NULL,
+    `capacity` int(11) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
