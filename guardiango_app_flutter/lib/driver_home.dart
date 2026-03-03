1913b25841d289a04416b4ace327dbd048f37a17
Induwari <Induwarikavindi@gmail.com>
2026-03-03 23:13:08 +0530

    a simple update driver home
    Statelesswidget

--- guardiango_app_flutter/lib/driver_home.dart
+++ guardiango_app_flutter/lib/driver_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class DriverhomeScreen extends StatelessWidget {
+class DriverhomeScreen extends StatefulWidget {
   const DriverhomeScreen({super.key});
 
   @override
