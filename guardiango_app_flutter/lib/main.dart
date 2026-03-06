5dceab5974bfca78b1fb09219886587edcafa8b7
Induwari <Induwarikavindi@gmail.com>
2026-03-06 23:48:21 +0530

    update main

--- guardiango_app_flutter/lib/main.dart
+++ guardiango_app_flutter/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart'; 
 import 'package:guardiango_app_flutter/splash_screen.dart'; 
 
+// 2. Add 'async' here
+void main() async { 
+  // 3. This is required when initializing things before runApp
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main()  {
+  // 4. Initialize Supabase
+  await Supabase.initialize(
+    url: 'https://qsnmxujrjedgqqryzwdb.supabase.co',        
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzbm14dWpyamVkZ3Fxcnl6d2RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NzE0NzQsImV4cCI6MjA4NzI0NzQ3NH0.q5aUgn1FBaMRio9tV1ysAJ2d5juxetu3f4SxIeSnFnI', // <-- Paste your actual Anon Key here
+  );
+
   runApp(const MyApp());
 }
 
@@ -18,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFEF3C7)),
         useMaterial3: true,
       ),
-      home: Splashscreen(), 
+      home: const Splashscreen(), 
     );
   }
 }
