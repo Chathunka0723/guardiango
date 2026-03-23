d5a71a335643100946ae224509697e70c18e1a1b
ThusaliVihasna <thusali.20242192@iit.ac.lk>
2026-03-23 11:25:54 +0530

    Add the Haptic Import

--- guardiango_app_flutter/lib/parent_emergency_contacts.dart
+++ guardiango_app_flutter/lib/parent_emergency_contacts.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
 
 class ParentEmergencyContact extends StatefulWidget {
   final bool scrollToFaq;
